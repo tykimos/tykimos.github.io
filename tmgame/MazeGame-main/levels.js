2c6016df0ac694b5d519b8cf3c73ebd31a3d1117
Taeyoung Kim <adam.tykim@gmail.com>
2022-02-12 20:06:55 +0900

    Add files via upload

---
+++ tmgame/MazeGame-main/levels.js
@@ -0,0 +1,120 @@
+var levelMatrix = {
+  "1": {
+    "name": "Level 1",
+    "start": {
+      "x": 0,
+      "y": 0
+    },
+    "rows": [
+      ";w;;w;;;;;;;;;w;;;;;;;;;;;;;;;;;;;;;;;;;;;b",
+      ";w;;w;;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;",
+      ";w;;w;;;;;;;;;;;;;;;;;;;;;;;;;;;;w;;;;;;;w;",
+      ";;;;;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;",
+      "w;;w;w;;;;;;;;w;;;;;;;;;;;b;;;;;;;;;w;;;;w;w;;w;",
+      "w;;w;;;w;w;w;w;w;w;w;w;w;w;;w;w;w;;w;w;w;w;w;w;w;w;w;w;;w;;w;;;w;;w;",
+      "w;;;;;w;;;;w;;;;;;;;;w;;w;;w;;w;;;;;;;w;;w;w;;w;;w;",
+      ";;;w;;w;;w;;w;;w;;w;w;w;w;;w;;w;;w;;;;w;w;w;w;;;;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;;;;w;;w;;w;;w;;w;;;;;;w;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;w;w;w;w;;w;;w;w;w;;w;w;w;w;w;;w;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;w;;;;;w;;;;w;;;w;;;;;w;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;w;;w;w;w;w;w;w;;w;w;;w;;w;w;;w;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;w;;w;;;;;;;;;b;;;w;;;w;;;;;;;",
+      ";w;;w;;;;w;;w;;w;;w;;w;;w;;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;;w;w;w;w",
+      ";w;;w;;w;;w;;;;w;;;;w;;;;w;;;;;;;;;;w;;;;;;;;;;",
+      ";w;;;;;;w;;w;w;w;w;w;w;w;w;w;;w;;w;w;w;w;w;w;w;;w;w;w;;w;;w;;w;w;w",
+      ";w;w;w;;w;w;w;w;w;;;;;;;;w;;w;;w;;;;;;;;w;;;;w;;w;;;;",
+      ";w;s;w;w;w;;;;w;w;w;w;w;w;w;;w;;w;;w;;w;w;w;w;w;w;w;;w;w;w;;w;w;w;w;w",
+      ";;;;w;;;w;;;;;;;;;;w;;;;w;;;;;;;;;;;;w;;;;;;",
+      "w;w;w;;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;x",
+    ],
+    "field": { }
+  },
+  "2": {
+    "name": "Level 2",
+    "start": {
+      "x": 0,
+      "y": 0
+    },
+    "rows": [
+      ";w;;w;;;;;;;;;w;;;;;;;;;;;;;;;;;;;;;;;;;;;",
+      ";w;;w;;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;",
+      ";w;;w;;;;;;;;w;;;;;;;;;;;;;;;;;;;s;w;;;;;;;w;",
+      ";;;;;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;",
+      "w;;w;w;;;;;;;;w;;;;;;;;;;;;;;;;;;;;w;;;;w;w;;w;",
+      "w;;w;;;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;;;w;;w;",
+      "w;;;;;w;;;;w;;;;;;;;;w;t(19,6);w;;w;;w;;;;;;;w;;w;w;;w;;w;",
+      ";;;w;;w;;w;;w;;w;;w;w;w;w;;w;;w;;w;;;;w;w;w;w;;;;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;;;;w;;w;;w;;w;;w;;;;;;w;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;w;w;w;w;;w;;w;w;w;;w;w;w;w;w;;w;;w;;w;;w;",
+      ";w;;w;;w;;w;;w;;w;;w;;w;;;;;w;;;;w;;;w;;;;;w;;w;;w;;w;t(3,19)",
+      ";w;;w;;w;;w;;w;;w;;w;;w;;w;w;w;w;w;w;;w;w;;w;;w;w;;w;;w;;w;;w;w",
+      ";w;;w;;w;;w;;w;;w;;w;;w;;w;;;;;;;;;;;;w;;;w;;;;;;;",
+      ";w;;w;;;;w;;w;;w;;w;;w;;w;;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;;w;w;w;w",
+      ";w;;w;;w;;w;;;;w;;;;w;;;;w;;;;;;;;;;w;;;;;;;;;;",
+      ";w;;;;;;w;;w;;w;w;w;w;w;w;w;;w;;w;w;w;w;w;w;w;;w;w;w;;w;b;w;;w;w;w",
+      ";w;w;w;;w;w;w;w;w;;;;;;;;w;;w;;w;;;;;;;;w;;;;w;;w;;;;",
+      ";w;;w;w;w;;;;w;w;w;w;w;w;w;;w;;w;;w;;w;w;w;w;w;w;w;;w;w;w;;w;w;w;w;w",
+      ";;;;w;;;w;;;;;;;;;;w;;;;w;;;;;;;;;;;;w;;;;;;",
+      "w;w;w;t(39,10);w;w;t(19,6);w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;x"
+    ],
+    "field": { }
+  },
+  "3": {
+    "name": "Level 3",
+    "start": {
+      "x": 6,
+      "y": 19
+    },
+    "rows": [
+      ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;",
+      ";w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w",
+      ";w;;;;;;;;;;;w;t(0,2);;;;;;;;;;;;s;;;;;;;;;;;w;t(35,2);w",
+      "w;w;;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w",
+      ";;;w;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;w;;w",
+      ";w;;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;;w",
+      ";w;;w;;w;;;;;;;;;;;;;;;;;;;;w;;;;;;;;;w;;w;;w",
+      ";w;;w;;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;x;w;w;w;w;;w;w;w;;w;;w;;w",
+      ";w;;w;;w;;w;;;;;;;;;;b;;w;w;w;;w;w;w;;;w;;;;w;;w;;w;;w",
+      ";w;;w;;w;;w;;w;w;w;w;w;w;w;;w;;;;;;;;;;;;;;;w;;w;;w;;w",
+      ";w;;w;;w;;b;;w;;;;;;;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;;w;;w;;w",
+      ";w;;w;;w;;w;;w;;;w;w;w;w;w;w;s;;;;;;;;;;;;;;w;;w;;w;;w",
+      ";w;;w;;w;;w;w;w;w;b;;w;;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;;w;;w",
+      ";w;;w;;w;;b;;;;;;;;;;w;;;;;;;;;;;;;;;;;w;;w;;w",
+      ";w;;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;;w",
+      ";w;;w;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;w;;w",
+      ";w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w",
+      ";w;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;w",
+      ";w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w;w",
+      ";;;;;;;w;t(26,6);;;;;;;;;w;t(0,19);;;;;;;;;;;;;;;;;;;;"
+    ]
+  },
+  "4": {
+    "name": "Level 4",
+    "start": {
+      "x": 12,
+      "y": 4
+    },
+    "rows": [
+      "w;w;t(8,8);;w;w;w;w;w;w;w;w;w;w;w;w;w;b;;;;;;w;;;;;;;w;w;w;w;w;;;;b;",
+      "w;;;;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;;w;;w;w;w;w;;w;w;;;;;b;;;x",
+      "w;w;;w;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;;w;;;;;w;;w;w;;w;w;;;;b;",
+      "w;;B(13,15);;w;w;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;;w;w;w;w;;w;;w;w;;w;w;;b;;;",
+      "w;;w;w;w;w;w;w;w;w;w;w;;;;w;w;;w;w;w;w;;;;;w;;w;;w;w;;w;w;w;w;w;w;w",
+      "w;;;s;w;w;w;w;w;w;w;w;w;;w;w;w;;;;w;w;w;w;w;;w;;w;;w;w;;;;;;;;w",
+      "w;w;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;;w;w;w;w;w;s;w;;w;;w;w;w;w;w;w;w;;;w",
+      "w;w;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;;w;w;w;w;w;;w;;w;;w;w;w;w;w;w;w;;;w",
+      "w;w;w;w;w;w;;;t(1,3);;;w;w;;w;w;w;w;w;;w;w;w;w;w;;w;;w;;;;;;w;w;w;;;w",
+      "w;w;w;w;w;w;;;;;;w;w;;w;w;w;e(r,r,r,r,l,l,l,l);;;;;w;w;w;;w;;w;w;w;w;w;;w;w;w;;;w",
+      "w;w;w;w;w;w;w;w;;w;w;w;e(r,r,l,l);;;w;w;w;w;;w;w;w;w;w;;w;;;;;;w;;w;w;w;b;;w",
+      "w;w;w;w;w;w;w;w;;w;w;w;w;;w;w;w;w;w;s;w;;;;;;w;w;w;w;w;;w;;w;w;w;;;w",
+      "w;w;w;w;w;w;w;w;;w;w;w;w;;w;w;w;w;w;;w;;w;w;w;w;w;b;;w;w;;w;;w;w;w;;;w",
+      ";;;;;;;;;w;w;w;w;;w;w;w;w;w;;w;;w;;;;;;t(27,17);w;w;;w;;w;w;w;;b;w",
+      ";w;w;w;w;w;w;w;w;w;w;w;w;;w;w;w;w;w;;w;;w;;w;w;w;;;w;w;;w;;w;w;w;;;w",
+      ";w;w;w;;;;w;w;w;w;;;;d;;w;w;w;;w;;w;;w;w;w;w;w;w;w;;w;;w;w;w;;;w",
+      ";w;w;w;;w;;w;w;w;w;;w;w;w;;w;w;w;;w;;w;;w;w;w;;b;w;w;;w;;w;w;w;b;;w",
+      ";w;w;w;;w;;w;w;w;w;;w;w;w;;w;w;w;;w;;w;;w;w;w;T(28,13);;;;;w;;w;w;w;;;w",
+      ";w;w;w;;w;;w;w;w;w;;w;w;w;;w;w;w;;w;;w;;w;w;w;;;w;w;w;w;;w;w;w;;;w",
+      ";;;;;w;;;;;;;w;w;w;b;;;;;w;;b;;w;w;w;w;w;w;w;w;w;;;;;;;w"
+    ]
+  }
+};
